539ac14d64b554664383aec8f8e2f656ad68fddb
Aidan Mariglia <aidan_m@outlook.com>
2024-11-13 21:24:45 -0500

    adding sidebar to submission app

--- src/webserver/webserver/webserver/urls.py
+++ src/webserver/webserver/webserver/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect('/authentication/')),
     path('accounts/', include('django.contrib.auth.urls')),
     path("submissions/", include("submissions.urls")),
     path("authentication/", include("authentication.urls")),
