be236c016acdb4da68554be0256c58f791ac5b03
Ben Dubois <duboisb@mcmaster.ca>
2024-11-13 19:25:49 -0500

    Making changes requested on PR

--- src/webserver/webserver/authentication/apps.py
+++ src/webserver/webserver/authentication/apps.py
@@ -4,16 +4,3 @@ from django.apps import AppConfig
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
-
-    # for dev purposes only. look into
-    # how we will preload data for production
-    # def ready(self):
-    #     from .models import SubmissionStatus
-
-    #     initial_statuses = [
-    #         {"name": "pending", "description": "placeholder"},
-    #         {"name": "failed", "description": "placeholder"},
-    #     ]
-
-    #     for status in initial_statuses:
-    #         SubmissionStatus.objects.get_or_create(name=status["name"], description=status["description"])
