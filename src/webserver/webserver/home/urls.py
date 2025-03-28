04d508dd59a792765de7810261095e4eb952ede3
Ben Dubois <duboisb@mcmaster.ca>
2025-03-27 22:37:14 -0400

    Adding functionality for additional help pages

--- src/webserver/webserver/home/urls.py
+++ src/webserver/webserver/home/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path("help/", views.help, name="help"),
     path("help/valid-files/", views.valid_files, name="valid_files"),
     path("help/code-ownership/", views.ownership, name="code_ownership"),
+    path("help/submission-process/", views.private, name="code_ownership"),
+    path("help/public-private-submission/", views.sub_process, name="code_ownership"),
+    path("help/account-verification/", views.verification, name="code_ownership"),
+    path("help/forgot-username/", views.forgot_username, name="code_ownership"),
+    path("help/forgot-password/", views.forgot_password, name="code_ownership"),
 ]
--- src/webserver/webserver/home/views.py
+++ src/webserver/webserver/home/views.py
@@ -37,6 +37,46 @@ def ownership(request: HttpRequest):
     else:
         return render(request, 'intro-ownership.html')
     
+def private(request: HttpRequest):
+    user = request.user
+
+    if request.user.is_authenticated:
+        return render(request, 'home-private-submission.html', {"user": user})
+    else:
+        return render(request, 'intro-private-submission.html')
+    
+def sub_process(request: HttpRequest):
+    user = request.user
+
+    if request.user.is_authenticated:
+        return render(request, 'home-sub-process.html', {"user": user})
+    else:
+        return render(request, 'intro-sub-process.html')
+    
+def verification(request: HttpRequest):
+    user = request.user
+
+    if request.user.is_authenticated:
+        return render(request, 'home-verification.html', {"user": user})
+    else:
+        return render(request, 'intro-verification.html')
+    
+def forgot_password(request: HttpRequest):
+    user = request.user
+
+    if request.user.is_authenticated:
+        return render(request, 'home-forgot-password.html', {"user": user})
+    else:
+        return render(request, 'intro-forgot-password.html')
+    
+def forgot_username(request: HttpRequest):
+    user = request.user
+
+    if request.user.is_authenticated:
+        return render(request, 'home-forgot-username.html', {"user": user})
+    else:
+        return render(request, 'intro-forgot-username.html')
+    
 def logout_view(request):
     logout(request)
     return render(request, 'intro.html')
